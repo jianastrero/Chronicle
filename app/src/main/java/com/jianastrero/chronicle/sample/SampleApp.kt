d82341502145efb12f68bc38b50d503107072047
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-20 15:06:05 +0800

    Initialize Chronicle as the Chronicles of Everything A fancy one to use: "Chronicles of Narnia"

--- app/src/main/java/com/jianastrero/chronicle/sample/SampleApp.kt
+++ app/src/main/java/com/jianastrero/chronicle/sample/SampleApp.kt
@@ -1,6 +1,13 @@
 package com.jianastrero.chronicle.sample
 
 import android.app.Application
+import com.jianastrero.chronicle.Chronicle
+import com.jianastrero.chronicle.stories.Everything
 
 class SampleApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Chronicle.of(Everything())
+    }
 }
