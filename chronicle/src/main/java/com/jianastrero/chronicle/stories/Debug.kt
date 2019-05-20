a5119afbaaae1b9e99dbba359193001a8e14d7f3
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-20 14:49:44 +0800

    Implement Story

--- chronicle/src/main/java/com/jianastrero/chronicle/stories/Debug.kt
+++ chronicle/src/main/java/com/jianastrero/chronicle/stories/Debug.kt
@@ -1,8 +1,12 @@
 package com.jianastrero.chronicle.stories
 
-class Debug {
+class Debug : Story {
 
     init {
 
+    }
+
+    override fun log(priority: Int, throwable: Throwable): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
