3ff1825f243bf207eb34b44dc316d1f4a1713655
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-20 15:53:57 +0800

    use d log for string parameter

--- app/src/main/java/com/jianastrero/chroniclesample/MainActivity.kt
+++ app/src/main/java/com/jianastrero/chroniclesample/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Chronicle.d(Throwable("Hello Errors"))
+        Chronicle.d("Hello Errors")
     }
 }
