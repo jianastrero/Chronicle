931ee0a07965973e66bf1cbe5ec0a91b4ecc454a
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-20 15:38:14 +0800

    Add sample to get key and rename it to get tag

--- chronicle/src/main/java/com/jianastrero/chronicle/Chronicle.kt
+++ chronicle/src/main/java/com/jianastrero/chronicle/Chronicle.kt
@@ -50,9 +50,29 @@ object Chronicle {
     /**
      * Private Methods
      */
-    private fun getKey(): String {
-        val stackTrace = Thread.currentThread().stackTrace;
-        Log.d("JIAN TEST", stackTrace.contentDeepToString())
-        return "";
+
+    /**
+     * [
+     *      dalvik.system.VMStack.getThreadStackTrace(Native Method),
+     *      java.lang.Thread.getStackTrace(Thread.java:1566),
+     *      com.jianastrero.chronicle.Chronicle.getKey(Chronicle.kt:54),
+     *      com.jianastrero.chronicle.Chronicle.d(Chronicle.kt:40),
+     *      com.jianastrero.chroniclesample.MainActivity.onCreate(MainActivity.kt:14),
+     *      android.app.Activity.performCreate(Activity.java:6679),
+     *      android.app.Instrumentation.callActivityOnCreate(Instrumentation.java:1118),
+     *      android.app.ActivityThread.performLaunchActivity(ActivityThread.java:2618),
+     *      android.app.ActivityThread.handleLaunchActivity(ActivityThread.java:2726),
+     *      android.app.ActivityThread.-wrap12(ActivityThread.java),
+     *      android.app.ActivityThread$H.handleMessage(ActivityThread.java:1477),
+     *      android.os.Handler.dispatchMessage(Handler.java:102),
+     *      android.os.Looper.loop(Looper.java:154),
+     *      android.app.ActivityThread.main(ActivityThread.java:6119),
+     *      java.lang.reflect.Method.invoke(Native Method),
+     *      com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:886),
+     *      com.android.internal.os.ZygoteInit.main(ZygoteInit.java:776)
+     * ]
+     */
+    private fun getTag(): String {
+
     }
 }
