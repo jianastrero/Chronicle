b84e7f23a5a03894e0ea479cef8df75766ed665c
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-20 14:35:45 +0800

    Create Chronicle object

---
+++ chronicle/src/main/java/com/jianastrero/chronicle/Chronicle.kt
@@ -0,0 +1,40 @@
+package com.jianastrero.chronicle
+
+object Chronicle {
+    /**
+     * Values declaration and initializatiom
+     */
+
+
+    /**
+     * Variables declaration and initialization
+     */
+
+
+    /**
+     * Object constructor initialization
+     */
+    init {
+
+    }
+
+
+    /**
+     * Overridden Methods
+     */
+
+
+    /**
+     * Public Methods
+     */
+
+
+    /**
+     * Protected Methods
+     */
+
+
+    /**
+     * Private Methods
+     */
+}
