a32884b2be2e88b1dc87b8d61d6b13a185f9c9c5
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-20 15:08:36 +0800

    Create a sample chronicle log

--- app/src/main/java/com/jianastrero/chronicle/sample/MainActivity.kt
+++ app/src/main/java/com/jianastrero/chronicle/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jianastrero.chronicle.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jianastrero.chronicle.Chronicle
 import com.jianastrero.chronicle.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Chronicle.d(Throwable("Hello Errors"))
     }
 }
