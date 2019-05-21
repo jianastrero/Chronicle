82ad860cf9c3200dd0ae00924d31f5b1141686d7
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-21 18:25:06 +0800

    Test if Debug Story works

--- app/src/main/java/com/jianastrero/chroniclesample/MainActivity.kt
+++ app/src/main/java/com/jianastrero/chroniclesample/MainActivity.kt
@@ -27,7 +27,6 @@ package com.jianastrero.chroniclesample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jianastrero.chronicle.Chronicle
-import com.jianastrero.chroniclesample.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +34,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Chronicle.d("Hello Errors")
+        Chronicle.v("Hello Errors")
     }
 }
