96d777964b8c0d373d747f204a2b8fb49647e9f5
Jian James Astrero <jianjamesastrero@gmail.com>
2019-05-22 00:32:39 +0800

    Log supporting character as table

--- chronicle/src/main/java/com/jianastrero/chronicle/characters/SupportingCharacter.kt
+++ chronicle/src/main/java/com/jianastrero/chronicle/characters/SupportingCharacter.kt
@@ -24,6 +24,7 @@
 
 package com.jianastrero.chronicle.characters
 
+import kotlin.math.max
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.jvm.isAccessible
@@ -35,13 +36,49 @@ abstract class SupportingCharacter(private val character: Any) {
 
     override fun toString(): String {
         updateTraits()
-        var str = " \n\n-${getName()}---"
 
-        traits.forEach {
-            str += "\n$it"
+        var cmcOne = getCellOneMaxCharacters()
+        var cmcTwo = getCellTwoMaxCharacters()
+        var cmcThree = getCellThreeMaxCharacters()
+
+        var str = "-${getName()}"
+
+        val missingDashesLength = cmcOne + cmcTwo + cmcThree + 2 - str.length
+        for (i in 1..max(missingDashesLength, 5))
+            str += "-"
+
+        val topLength = str.length
+
+        if (topLength > cmcOne + cmcTwo + cmcThree + 2) {
+            val additional = (topLength - cmcOne - cmcTwo - cmcThree - 2) / 3
+            val additionalThree = topLength - cmcOne - cmcTwo - cmcThree - 2 - additional * 2
+            cmcOne += additional
+            cmcTwo += additional
+            cmcThree += additionalThree
         }
 
-        str += "\n "
+        str = " \n\n|$str|"
+
+        traits.add(0, Trait("TYPE", "NAME", "VALUE"))
+
+        traits.forEachIndexed { index, item ->
+            str += "\n|${item.toRow(cmcOne, cmcTwo, cmcThree)}|"
+            if (index == 0) {
+                str += "\n|"
+
+                for (i in 1..topLength)
+                    str += "-"
+
+                str += "|"
+            }
+        }
+
+        str += "\n|"
+
+        for (i in 1..topLength)
+            str += "-"
+
+        str += "|\n "
 
         return str
     }
@@ -58,20 +95,68 @@ abstract class SupportingCharacter(private val character: Any) {
         }
     }
 
-    private fun getMaxLineCharacters(): Int {
-        var max = getName().length
+    private fun getCellOneMaxCharacters(): Int {
+        var max = "NAME".length
 
         traits.forEach {
-            if (it.toString().length > max)
-                max = it.toString().length
+            if (it.name.length > max)
+                max = it.name.length
         }
 
-        return max
+        return max + 2
+    }
+
+    private fun getCellTwoMaxCharacters(): Int {
+        var max = "VALUE".length
+
+        traits.forEach {
+            if (it.value.length > max)
+                max = it.value.length
+        }
+
+        return max + 2
+    }
+
+    private fun getCellThreeMaxCharacters(): Int {
+        var max = "CLASS / TYPE".length
+
+        traits.forEach {
+            if (it.type.length > max)
+                max = it.type.length
+        }
+
+        return max + 2
     }
 
     class Trait(val type: String, val name: String, val value: String) {
         override fun toString(): String {
             return "$name($value): $type"
+        }
+
+        fun toRow(
+            cellOneCharCount: Int,
+            cellTwoCharCount: Int,
+            cellThreeCharCount: Int
+        ): String {
+            return addSpacesToCenter(name, cellOneCharCount) + "|" +
+                    addSpacesToCenter(value, cellTwoCharCount) + "|" +
+                    addSpacesToCenter(type, cellThreeCharCount)
+        }
+
+        private fun addSpacesToCenter(string: String, length: Int): String {
+            val remainingLength = length - string.length
+            val leftSpaces = remainingLength / 2
+            val rightSpaces = remainingLength - leftSpaces
+
+            var str = string
+
+            for (i in 1..leftSpaces)
+                str = " $str"
+
+            for (i in 1..rightSpaces)
+                str += " "
+
+            return str
         }
     }
 }
